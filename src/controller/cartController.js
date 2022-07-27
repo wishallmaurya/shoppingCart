58e4c289e8f8849c1b6389c29f6a9254b8121980
Vishal Kumar Maurya <105576405+wishallmaurya@users.noreply.github.com>
2022-07-27 16:49:32 +0530

    only delete api

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,1 +1,23 @@
-//hfjfkjgkj
+//only delete 
+
+const deleteCart = async function (req, res) {
+    try {
+        const userId = req.params.userId
+        if (userId.trim().length == 0)
+	      return res.status(400).send({ status: false, Message: "Dont Left userId Empty" })
+
+        if (!mongoose.isValidObjectId(userId)) 
+	      return res.status(400).send({ status: true, Message: "Invalid ProductId !" })
+     
+        const checkCart = await cartModel.findOne({ userId: userId })
+        if (!checkCart) 
+        return res.status(400).send({ status: false, Message: 'cart not found ' })
+       
+       if (checkCart.items.length==0)
+	     return res.status(400).send({ status: false, Message: "Cart is already empty" })
+      
+       await cartModel.findOneAndUpdate({ userId: userId }, { items: [], totalPrice: 0, totalItems: 0 })
+       res.status(200).send({ status: true, Message: 'sucessfully deleted' })
+    } catch (error) { 
+      res.status(500).send({ status: false, Message: error.message }) }
+}
